9de78684460d1586107892707a8ba5703a404f6a
AoflJ <aof.kurosung@gmail.com>
2018-12-17 17:25:27 +0700

    Add author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -8,12 +8,22 @@ class Question extends Model
 {
     protected $fillable = ['title','body'];
     public function user(){
-        return $this->belongsTo(Usere::class);
+        return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
